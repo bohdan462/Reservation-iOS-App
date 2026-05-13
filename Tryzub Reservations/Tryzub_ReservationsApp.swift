fc406a18f5481cccf16a453bfadcd06dc8b629c1
Bohdan Solovey <30517042+bohdan462@users.noreply.github.com>
2026-05-13 16:00:27 -0500

    Initial Commit

---
+++ Tryzub Reservations/Tryzub_ReservationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tryzub_ReservationsApp.swift
+//  Tryzub Reservations
+//
+//  Created by Bohdan Tkachenko on 5/13/26.
+//
+
+import SwiftUI
+
+@main
+struct Tryzub_ReservationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
